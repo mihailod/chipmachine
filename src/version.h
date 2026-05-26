bf4358daf68ea25e0de1352cc7fe418adf6d0efa
Mihailo Despotovic <mihailod@me.com>
2026-05-26 10:19:35 -0700

    Add mock version instructions

--- src/version.h
+++ src/version.h
@@ -1,3 +1,16 @@
+/*
+
+Simulating a newer version without GitHub:
+
+cd /Users/mihailod/Documents/chipmachine-as/build
+echo "9.9.9" > DEBUG_UPDATE_VERSION
+./chipmachine
+
+After testing:
+rm build/DEBUG_UPDATE_VERSION
+
+*/
+
 #pragma once
 #define PROGRAM_NAME "ChipMachineAS"
 #define VERSION_STR "1.0.4"
